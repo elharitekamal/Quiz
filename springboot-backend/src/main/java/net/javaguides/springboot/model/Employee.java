07d53d9702ac794300d8c24c6f523f601d64161b
elharitekamal <abdelkhalekelharite@gmail.com>
2023-12-28 22:45:27 +0100

    back

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,25 @@
+package net.javaguides.springboot.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
